305a4faa8e550b118c22887ac30d1b6e13369e3a
Alan Ho <alanho@gmail.com>
2020-06-20 23:42:02 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,66 @@
-import { API } from 'homebridge';
+import {
+  API,
+  // CharacteristicEventTypes,
+  // CharacteristicGetCallback,
+  // CharacteristicSetCallback,
+  // CharacteristicValue,
+  HAP,
+  IndependentPlatformPlugin,
+  Logging,
+  PlatformAccessory,
+  PlatformConfig,
+} from 'homebridge';
+
 
 import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+import ledshim from 'ledshim';
 
-/**
- * This method registers the platform with Homebridge
- */
+let hap: HAP;
+let Accessory: typeof PlatformAccessory;
+
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  hap = api.hap;
+  Accessory = api.platformAccessory;
+
+  api.registerPlatform(PLATFORM_NAME, LEDShimPlatform);
+};
+
+class LEDShimPlatform implements IndependentPlatformPlugin {
+  private intervalID?: NodeJS.Timeout;
+
+  constructor(
+    public readonly log: Logging,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log = log;
+    this.api = api;
+
+    this.intervalID = undefined;
+
+    log('Finished initializing platform:', this.config.name);
+
+    this.api.on('didFinishLaunching', () => {
+      if (this.intervalID) {
+        clearInterval(this.intervalID);
+      }
+
+
+      this.intervalID = setInterval(() => {
+        this.updateDisplay();
+      }, 500);
+    });
+  }
+
+  updateDisplay(): void {
+    for (let i = 0; i < 28; i++) {
+      ledshim.setPixel(
+        i,
+        Math.floor((Math.random() * 255)),
+        Math.floor((Math.random() * 255)),
+        Math.floor((Math.random() * 255)),
+        0.5);  // brightness
+    }
+    ledshim.show();
+  }
 }
